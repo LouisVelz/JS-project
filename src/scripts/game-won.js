2aa785cdfc455c77fa50ddd9a107416d372324e5
LouisVelz <velazquezlouis2@gmail.com>
2020-09-04 00:38:47 -0400

    fix game won logic

--- src/scripts/game-won.js
+++ src/scripts/game-won.js
@@ -1,12 +1,11 @@
 export const gameOver = () => {
   let puzzle = document.querySelectorAll("[class^='square']");
 
-  for (let index = 0; index < puzzle.length; index++) {
-    const element = Number (puzzle[index].innerHTML);
-
-    if ((element - 1)!== index) return false
-    
+  for (let index = 0; index < 15; index++) {
+    const element = Number(puzzle[index].attributes.class.nodeValue.slice(6));
+    debugger;
+    if (element - 1 !== index) return false;
   }
-
-  return true
-}
+  debugger;
+  return true;
+};
